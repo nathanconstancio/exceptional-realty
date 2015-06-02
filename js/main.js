f06d4aaae3b7c71c0f0892c30afddccd4064cdb2
NATHAN SANCHEZ <nathansanchez@NATHANs-Air.home>
2015-06-02 12:10:49 -0400

    added media query and js for mobile nav menu

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+
+
+	//////MOBILE MENU//////////
+
+	$('a.menu-icon').click(function(){
+
+		$('#menu-links').slideToggle();
+	});
+
+	// fix hidden links on window resize
+
+	$(window).resize(function() {
+
+		if ($(window).width() > 700) {
+			$('#menu-links').removeAttr('style');
+		}
+	})
+
+});
